6e4452fd41434ad18242f21fedca98e2c9ef38a8
dugloglu <126673501+dugloglu@users.noreply.github.com>
2024-06-27 05:58:07 +0300

    u

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const nextConfig = {
     config.plugins.push(new NodePolyfillPlugin());
     return config;
   },
+  exportTrailingSlash: true,
+  images: {
+    unoptimized: true, // Отключает оптимизацию изображений, если это требуется
+  },
+  async redirects() {
+    return [
+      {
+        source: '/feedBackForm',
+        destination: '/#feedBackForm', // Обратите внимание на правописание в destination
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
